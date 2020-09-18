f68ebc5a32f0897565f59d2cb5d58884ada80b64
kalyan287 <kalyansai287@gmail.com>
2020-09-18 18:06:02 +0530

    Updates

---
+++ precache-manifest.a11f8c254a1686a3336b763225f93ed8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c1e9739642f8e6da5a29b67d659ca4",
+    "url": "/my-demo/index.html"
+  },
+  {
+    "revision": "d7da51f0d02acce3b405",
+    "url": "/my-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3472aa9d62c0857e5c7",
+    "url": "/my-demo/static/js/2.6e173309.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-demo/static/js/2.6e173309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7da51f0d02acce3b405",
+    "url": "/my-demo/static/js/main.57f7e299.chunk.js"
+  },
+  {
+    "revision": "8b97fb0dd981a16ccff1",
+    "url": "/my-demo/static/js/runtime-main.71a242ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
